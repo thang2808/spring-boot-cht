84445b99fdb6b454122f4a25a2442fd3531d8e01
hungthang <chthang1007@gmail.com>
2024-09-27 16:41:24 +0700

    new

--- src/main/java/com/digidinos/shopping/repository/OrderRepository.java
+++ src/main/java/com/digidinos/shopping/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.digidinos.shopping.repository;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 
 import com.digidinos.shopping.entity.Order;
 
@@ -15,4 +17,5 @@ public interface OrderRepository extends JpaRepository<Order, String> {
 	
 	// Tìm kiếm đơn hàng theo email khách hàng và có phân trang
     Page<Order> findByCustomerEmail(String email, Pageable pageable);
+   
 }
